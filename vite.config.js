a088a5dae73f3ddfd4e5e2a15ff8857b6c129f53
Wyatt H <jhawes@ucsc.edu>
2023-12-10 14:34:18 -0800

    attempt #5

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 export default {
   // eslint-disable-next-line no-undef
-  base: process.env.REPO_NAME || "/repo-name/",
+  //base: process.env.REPO_NAME || "/repo-name/",
+  base: "/repo-name/",
   manifest: true,
 };
