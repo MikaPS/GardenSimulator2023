648a924caa3de9ce71296f3862042c720899fe6c
Wyatt H <jhawes@ucsc.edu>
2023-12-11 20:33:37 -0800

    Another attempt

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,30 @@
-export default {
-  // eslint-disable-next-line no-undef
-  base: process.env.REPO_NAME || "repo-name",
+import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: "repo-name",
   manifest: true,
-};
+  plugins: [
+    VitePWA({
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+      manifest: {
+        name: "My Awesome App",
+        short_name: "MyApp",
+        description: "My Awesome App description",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
