c7546ba8d74efd821625ee6943cb708d61be76f6
Wyatt H <jhawes@ucsc.edu>
2023-12-12 19:18:58 -0800

    Pushing built Vite app that is downloadable

--- src/assets/translations.ts
+++
@@ -1,6 +0,0 @@
-/* eslint-disable @typescript-eslint/naming-convention */
-export const LevelCompleteTranslation: Record<string, string> = {
-  English: "Level \nComplete!",
-  Hebrew: "!עברת שלב",
-  Chinese: "关卡完成!",
-};
