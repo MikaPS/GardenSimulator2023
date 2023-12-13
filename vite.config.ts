782ef328868b412f38698dbbb8abe654ed09d81b
Wyatt H <jhawes@ucsc.edu>
2023-12-13 01:36:39 -0800

    (Working build) run dev (no download), npx vit (everything), github pages (everything)

--- vite.config.ts
+++ vite.config.ts
@@ -2,22 +2,23 @@ import { VitePWA } from "vite-plugin-pwa";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  publicDir: "public",
   base: process.env.REPO_NAME || "",
+  publicDir: process.env.REPO_NAME || "public",
+
   plugins: [
     VitePWA({
       manifest: {
         icons: [
           {
-            src: "../assets/tiny_turnip_512.png",
+            src: "assets/tiny_turnip_512.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "any maskable",
           },
           {
-            src: "favicon.ico", // Adjust the path based on your project structure
+            src: "favicon.ico",
             sizes: "512x512",
-            type: "image/x-icon", // Favicon is typically of type 'image/x-icon'
+            type: "image/x-icon",
             purpose: "any maskable",
           },
         ],
@@ -26,7 +27,7 @@ export default defineConfig({
         runtimeCaching: [
           {
             urlPattern: ({ url }) => {
-              return url.pathname.startsWith("");
+              return url.pathname.startsWith("/");
             },
             handler: "CacheFirst" as const,
             options: {
@@ -36,6 +37,18 @@ export default defineConfig({
               },
             },
           },
+          // {
+          //   urlPattern: ({ url }) => {
+          //     return url.pathname.startsWith("/");
+          //   },
+          //   handler: "CacheFirst" as const,
+          //   options: {
+          //     cacheName: "api-cache",
+          //     cacheableResponse: {
+          //       statuses: [0, 200],
+          //     },
+          //   },
+          // },
         ],
       },
     }),
