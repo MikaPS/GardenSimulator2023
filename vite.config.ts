10e66450517a20378bd8330a69641a1816031785
Wyatt H <jhawes@ucsc.edu>
2023-12-12 19:13:24 -0800

    Pushing new vite config
    Co-authored-by: Lyn-G <Lyn-G@users.noreply.github.com>
    Co-authored-by: MikaPS <MikaPS@users.noreply.github.com>
    Co-authored-by: zchaplin <zchaplin@users.noreply.github.com>

---
+++ vite.config.ts
@@ -0,0 +1,46 @@
+import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from "vite";
+import { fileURLToPath, URL } from "node:url";
+
+export default defineConfig({
+  //publicDir: "./public/www",
+  plugins: [
+    VitePWA({
+      manifest: {
+        icons: [
+          {
+            src: "../assets/tiny_turnip_512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: ({ url }) => {
+              return url.pathname.startsWith("/");
+            },
+            handler: "CacheFirst" as const,
+            options: {
+              cacheName: "api-cache",
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+            },
+          },
+        ],
+      },
+    }),
+  ],
+  build: {
+    outDir: "../wwwroot/",
+    emptyOutDir: true,
+  },
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
