905a2aa79fe1569723bec309a2d2bb8e8a6b62f6
Wyatt H <jhawes@ucsc.edu>
2023-12-12 21:11:01 -0800

    Making base empty

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/repo-name/sw.js', { scope: '/repo-name/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
