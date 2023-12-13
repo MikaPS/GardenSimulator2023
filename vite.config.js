b1b3a2ed0bc694d8e7795bc3cb3fdc767a697676
MikaPS <mikaps7@gmail.com>
2023-12-12 16:41:09 -0800

    another test

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,19 @@
-// import { VitePWA } from "vite-plugin-pwa";
+import { VitePWA } from "vite-plugin-pwa";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [
-    // VitePWA(),
-    // {
-    //   manifest: {
-    //     icons: [
-    //       {
-    //         src: "/assets/tiny_turnip_512.png",
-    //         sizes: "512x512",
-    //         type: "image/png",
-    //         purpose: "any maskable",
-    //       },
-    //     ],
-    //   },
-    // },
+    VitePWA({
+      manifest: {
+        icons: [
+          {
+            src: "/assets/tiny_turnip_512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
   ],
 });
